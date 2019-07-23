96038b3299bb841f74dd7d443b7e33ef26c8316e
Synozure <kainevangemert@gmail.com>
2019-07-24 05:58:52 +1000

    Version 0.2.0-Alpha
    Updated to use new processor generator

--- AutoSplit64.py
+++ AutoSplit64.py
@@ -8,184 +8,73 @@ from as64core.resource_utils import resource_path
 from as64gui.app import App
 
 import as64core
-from as64core.processing import Processor, Transition
-from as64core.processes import (
-    ProcessFadein,
-    ProcessFadeout,
-    ProcessPostFadeout,
-    ProcessFlashCheck,
-    ProcessReset,
-    ProcessStarCount,
-    ProcessRunStart,
-    ProcessFinalStageEntry,
-    ProcessFinalStarSpawn,
-    ProcessFinalStarGrab,
-    ProcessFindDDDPortal,
-    ProcessDDDEntry,
-    ProcessLBLJ,
-    ProcessIdle
-)
+from as64core.processing import register_process, ProcessorGenerator
+from as64core.updater import Updater
+
+from as64processes.standard import *
+from as64processes.lblj import *
+from as64processes.ddd import *
+from as64processes.final import *
 
 
 class AutoSplit64(QtCore.QObject):
     error = QtCore.pyqtSignal(str)
+    update_found = QtCore.pyqtSignal(dict)
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
 
         # Initialize GUI
         self.app = App()
+
+        # Initialize updater
+        self._updater = Updater()
+        self._updater.set_exit_listener(self)
+
+        # Connections
         self.app.start.connect(lambda: Thread(target=self.start).start())
         self.app.stop.connect(self.stop)
         self.app.destroyed.connect(self.stop)
+        self.app.check_update.connect(lambda: Thread(target=self.check_for_update, args=(True,)).start())
+        self.app.ignore_update.connect(lambda: self._updater.set_ignore_update(True))
+        self.app.install_update.connect(self._updater.install_update)
         self.error.connect(self.app.display_error_message)
+        self.update_found.connect(self.app.update_found)
+
+        # Check for updates
+        Thread(target=self.check_for_update).start()
+
+    def check_for_update(self, override_ignore=False):
+        if self._updater.check_for_update(override_ignore):
+            self.update_found.emit({"current": self._updater.current_version_info(), "latest": self._updater.latest_version_info()})
 
     def start(self):
         as64core.init()
 
-        # TODO: Generate transitions from file
-        main_processor = Processor()
-        main_init_processor = Processor()
-        main_run_processor = Processor()
+        register_process("WAIT", ProcessWait())
+        register_process("RUN_START", ProcessRunStart())
+        register_process("STAR_COUNT", ProcessStarCount())
+        register_process("FADEIN", ProcessFadein())
+        register_process("FADEOUT", ProcessFadeout())
+        register_process("POST_FADEOUT", ProcessPostFadeout())
+        register_process("FLASH_CHECK", ProcessFlashCheck())
+        register_process("RESET", ProcessReset())
 
-        ddd_processor = Processor()
-        ddd_portal_processor = Processor()
-        ddd_entry_processor = Processor()
+        register_process("FIND_DDD_PORTAL", ProcessFindDDDPortal())
+        register_process("DDD_SPLIT", ProcessDDDEntry())  # TODO: RENAME ProcessDDDEntry to ProcessDDDSplit
 
-        lblj_processor = Processor()
+        register_process("FINAL_DETECT_ENTRY", ProcessFinalStageEntry())  # TODO: RENAME
+        register_process("FINAL_DETECT_SPAWN", ProcessFinalStarSpawn())  # TODO: RENAME
+        register_process("FINAL_STAR_SPLIT", ProcessFinalStarGrab())  # TODO: RENAME to FINAL_STAR_SPLIT
 
-        final_processor = Processor()
-        final_entry_processor = Processor()
-        final_spawn_processor = Processor()
-        final_grab_processor = Processor()
-        final_idle_processor = Processor()
+        standard_processor = ProcessorGenerator.generate("logic/standard.processor")
+        ddd_processor = ProcessorGenerator.generate("logic/ddd/ddd.processor")
+        final_processor = ProcessorGenerator.generate("logic/final/final.processor")
 
-        # self.core
-
-        # Regular Processes
-        process_fadein = ProcessFadein()
-        process_fadeout = ProcessFadeout()
-        process_reset = ProcessReset()
-        process_run_start = ProcessRunStart()
-        process_star_count = ProcessStarCount()
-        process_post_fadeout = ProcessPostFadeout()
-        process_flash_check = ProcessFlashCheck()
-        process_idle = ProcessIdle()
-
-        # DDD Processes
-        process_ddd_portal = ProcessFindDDDPortal()
-        process_ddd_entry = ProcessDDDEntry()
-
-        # LBLJ Processes
-        process_lblj = ProcessLBLJ()
-
-        # final bowser processes
-        process_final_stage_entry = ProcessFinalStageEntry()
-        process_final_star_spawn = ProcessFinalStarSpawn()
-        process_final_star_grab = ProcessFinalStarGrab()
-
-        # Set initial processes
-        main_processor.initial_process = main_init_processor
-        main_init_processor.initial_process = process_run_start
-        main_run_processor.initial_process = process_star_count
-
-        ddd_processor.initial_process = ddd_portal_processor
-        ddd_portal_processor.initial_process = process_ddd_portal
-        ddd_entry_processor.initial_process = process_ddd_entry
-
-        final_processor.initial_process = final_entry_processor
-        final_entry_processor.initial_process = process_final_stage_entry
-        final_spawn_processor.initial_process = process_final_star_spawn
-        final_grab_processor.initial_process = process_final_star_grab
-        final_idle_processor.initial_process = process_idle
-
-        # MAIN PROCESSOR TRANSITIONS
-        main_processor.add_transition(Transition(main_init_processor, ProcessRunStart.START, main_run_processor))
-        main_processor.add_transition(Transition(main_run_processor, ProcessReset.RESET, main_init_processor))
-
-        #
-        # INIT_PROCESSOR TRANSITIONS
-        #
-        main_init_processor.add_transition(Transition(process_run_start, ProcessRunStart.FADEOUT, process_fadeout))
-        main_init_processor.add_transition(Transition(process_fadeout, ProcessFadeout.COMPLETE, process_run_start))
-        main_init_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-        main_init_processor.add_transition(Transition(process_reset, ProcessReset.RESET, process_run_start))
-
-        #
-        # RUN_PROCESSOR TRANSITIONS
-        #
-        # Star count
-        main_run_processor.add_transition(Transition(process_star_count, ProcessStarCount.FADEOUT, process_fadeout))
-        main_run_processor.add_transition(Transition(process_star_count, ProcessStarCount.FADEIN, process_fadein))
-
-        # Fadeout
-        main_run_processor.add_transition(Transition(process_fadeout, ProcessFadeout.COMPLETE, process_post_fadeout))
-        main_run_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-
-        # Fadein
-        main_run_processor.add_transition(Transition(process_fadein, ProcessFadein.COMPLETE, process_star_count))
-
-        # Post Fadeout
-        main_run_processor.add_transition(Transition(process_post_fadeout, ProcessPostFadeout.FADEOUT, process_fadeout))
-        main_run_processor.add_transition(Transition(process_post_fadeout, ProcessPostFadeout.FADEIN, process_fadein))
-        main_run_processor.add_transition(Transition(process_post_fadeout, ProcessPostFadeout.FLASH, process_flash_check))
-        main_run_processor.add_transition(Transition(process_post_fadeout, ProcessPostFadeout.COMPLETE, process_star_count))
-
-        # Flash Check
-        main_run_processor.add_transition(Transition(process_flash_check, ProcessFlashCheck.FADEOUT, process_fadeout))
-        main_run_processor.add_transition(Transition(process_flash_check, ProcessFlashCheck.FADEIN, process_fadein))
-        main_run_processor.add_transition(Transition(process_flash_check, ProcessFlashCheck.COMPLETE, process_star_count))
-
-        #
-        # FINAL_BOWSER_PROCESSOR TRANSITIONS
-        #
-        final_entry_processor.add_transition(Transition(process_final_stage_entry, ProcessFinalStageEntry.FADEOUT, process_fadeout))
-        final_entry_processor.add_transition(Transition(process_fadeout, ProcessFadeout.COMPLETE, process_final_stage_entry))
-        final_entry_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-
-        final_spawn_processor.add_transition(Transition(process_final_star_spawn, ProcessFinalStarSpawn.FADEOUT, process_fadeout))
-        final_spawn_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-
-        final_grab_processor.add_transition(Transition(process_final_star_grab, ProcessFinalStarGrab.FADEOUT, process_fadeout))
-        final_grab_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-
-        final_idle_processor.add_transition(Transition(process_idle, ProcessIdle.FADEOUT, process_fadeout))
-        final_idle_processor.add_transition(Transition(process_fadeout, process_fadeout.RESET, process_reset))
-        final_idle_processor.add_transition(Transition(process_fadeout, process_fadeout.COMPLETE, process_idle))
-
-        final_processor.add_transition(Transition(final_entry_processor, ProcessFinalStageEntry.ENTERED, final_spawn_processor))
-        final_processor.add_transition(Transition(final_spawn_processor, ProcessFadeout.COMPLETE, final_entry_processor))
-        final_processor.add_transition(Transition(final_spawn_processor, ProcessFinalStarSpawn.SPAWNED, final_grab_processor))
-        final_processor.add_transition(Transition(final_grab_processor, ProcessFadeout.COMPLETE, final_entry_processor))
-        final_processor.add_transition(Transition(final_grab_processor, ProcessFadeout.COMPLETE, final_entry_processor))
-        final_processor.add_transition(Transition(final_grab_processor, ProcessFinalStarGrab.COMPLETE, final_idle_processor))
-
-        #
-        # DDD Processor
-        #
-        ddd_portal_processor.add_transition(Transition(process_ddd_portal, ProcessFindDDDPortal.FADEOUT, process_fadeout))
-        ddd_portal_processor.add_transition(Transition(process_fadeout, ProcessFadeout.COMPLETE, process_ddd_portal))
-        ddd_portal_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-
-        ddd_entry_processor.add_transition(Transition(process_ddd_entry, ProcessDDDEntry.FADEOUT, process_fadeout))
-        ddd_entry_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-
-        ddd_processor.add_transition(Transition(ddd_portal_processor, ProcessFindDDDPortal.FOUND, ddd_entry_processor))
-        ddd_processor.add_transition(Transition(ddd_entry_processor, ProcessFadeout.COMPLETE, ddd_portal_processor))
-
-        #
-        # LBLJ Processor
-        #
-        lblj_processor.initial_process = process_lblj
-        lblj_processor.add_transition(Transition(process_lblj, ProcessLBLJ.FADEOUT, process_fadeout))
-        lblj_processor.add_transition(Transition(process_fadeout, ProcessFadeout.COMPLETE, process_lblj))
-        lblj_processor.add_transition(Transition(process_fadeout, ProcessFadeout.RESET, process_reset))
-        lblj_processor.add_transition(Transition(process_reset, ProcessReset.RESET, process_lblj))
-
-        as64core.register_split_processor(as64core.SPLIT_NORMAL, main_processor)
-        as64core.register_split_processor("SPLIT_LBLJ", lblj_processor)
+        as64core.register_split_processor(as64core.SPLIT_NORMAL, standard_processor)
         as64core.register_split_processor(as64core.SPLIT_MIPS, ddd_processor)
         as64core.register_split_processor(as64core.SPLIT_FINAL, final_processor)
+
         as64core.set_update_listener(self.on_update)
         as64core.set_error_listener(self.on_error)
         as64core.start()
@@ -202,6 +91,11 @@ class AutoSplit64(QtCore.QObject):
         self.error.emit(error)
         self.app.set_started(False)
 
+    def exit(self):
+        print("EXIT!")
+        self.stop()
+        self.app.close()
+
 
 if __name__ == "__main__":
     # Create QT Application
@@ -211,20 +105,21 @@ if __name__ == "__main__":
     qt_app.setStyle('Fusion')
 
     palette = QtGui.QPalette()
-    palette.setColor(QtGui.QPalette.Window, QtGui.QColor(36, 38, 50))
-    palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(182, 193, 221))
-    palette.setColor(QtGui.QPalette.Base, QtGui.QColor(15, 23, 30))
+    palette.setColor(QtGui.QPalette.Window, QtGui.QColor(60, 63, 65))
+    palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(200, 203, 207))
+    palette.setColor(QtGui.QPalette.Link, QtGui.QColor(88, 157, 246))
+    palette.setColor(QtGui.QPalette.Base, QtGui.QColor(23, 25, 27))
     palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 55, 57))
     palette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.white)
     palette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
-    palette.setColor(QtGui.QPalette.Text, QtCore.Qt.white)
+    palette.setColor(QtGui.QPalette.Text, QtGui.QColor(200, 203, 207))
     palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 55, 57))
-    palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(212, 223, 251))
+    palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(200, 203, 207))
     palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
     palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(75, 110, 175))
     palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
     palette.setColor(QtGui.QPalette.Light, QtGui.QColor(105, 108, 112))
-    palette.setColor(QtGui.QPalette.Dark, QtGui.QColor(12, 14, 16))
+    palette.setColor(QtGui.QPalette.Dark, QtGui.QColor(12, 12, 12))
 
     qt_app.setPalette(palette)
 
