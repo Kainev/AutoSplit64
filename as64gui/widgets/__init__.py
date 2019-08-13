2f8709f577f0b60b02450525577e558f91b76367
Synozure <kainevangemert@gmail.com>
2019-08-13 17:01:42 +1000

    Version 0.2.3
      - Hot-fix: Update to reset detection to increase reliability

--- as64gui/widgets/__init__.py
+++ as64gui/widgets/__init__.py
@@ -2,5 +2,4 @@ from ..widgets.table_widget import TableWidgetDragRows
 from ..widgets.picture_button import PictureButton, StateButton
 from ..widgets.split_list import SplitListWidget
 from ..widgets.misc import StarCountDisplay, HLine
-from ..widgets.notes_widget import NotesWidget
 
