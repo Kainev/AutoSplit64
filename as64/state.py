a29507dfcdc8b4da058b59b7a0173232a7af2992
Kaine <kainevangemert@gmail.com>
2022-06-22 11:19:07 +1000

    base state machine system

--- as64/state.py
+++ as64/state.py
@@ -1,32 +1,83 @@
+from typing import Any, Dict, List
+
+
 class State(object):
     def __init__(self):
+        self._transitions: Dict[Any : State] = {}
+
+    def on_enter(self, sm, ev):
         pass
 
-    def on_enter(self):
+    def on_update(self, sm, ev):
         pass
 
-    def on_update(self):
+    def on_exit(self, sm, ev):
         pass
-
-    def on_exit(self):
-        pass
-
-
-class Transition(object):
-    def __init__(self):
-        self._
+          
+    def _add_transition(self, destination, signal: Any):
+        self._transitions[signal] = destination
 
 
 class StateMachine(object):
-    def __init__(self):
+    def __init__(self, event):
         self._initial_state: State = None
         self._current_state: State = None
-        self._states: list = []
+        self._states: list[State] = []
+        
+        self._event = event
+        
+        self._global_transitions = {}
 
-        self._transitions = {}
+    def add_transition(self, source: State, destination: State, signal: Any):
+        # Register states with StateMachine
+        if source and source not in self._states:
+            self._add_state(source)
+        if destination not in self._states:
+            self._add_state(destination)
+        
+        # If no source state is provided, add as global transition        
+        if not source:
+            # TODO: Throw error if signal is already a global transition
+            self._global_transitions[signal] = destination
+            
+            for state in self._states:
+                if signal not in state._transitions.keys():
+                    state._add_transition(destination, signal)
+        else:
+            source._add_transition(destination, signal)
+          
+    def _add_state(self, state: State):
+        self._states.append(state)
+        
+        for transition in self._global_transitions:
+            state._add_transition(self._global_transitions[transition], transition)
+                       
+    def set_initial_state(self, state: State):
+        # Ensure state is registered
+        if state not in self._states:
+            self._add_state(state)
+            
+        # Set initial state
+        self._initial_state = state
+        
+        # On first call, set current state to initial state
+        if not self._current_state:
+            self._current_state = self._initial_state
+            
+    def trigger(self, signal: Any):
+        # Ignore internal transitions
+        print("TRIGGER", self._current_state._transitions)
+        if self._current_state._transitions[signal] == self._current_state:
+            return
+        
+        # Transition to new state
+        self._current_state.on_exit(self, self._event)
+        self._current_state: State = self._current_state._transitions[signal]
+        self._current_state.on_enter(self, self._event)
+        
+    def update(self):
+        self._current_state.on_update(self, self._event)
 
-    def add_transition(source, destination, signal):
-        pass
 
 
 
