de61bb19e537e1c0953b767975b3d8bb6ffacfcf
Kaine <kainevangemert@gmail.com>
2022-06-28 16:58:25 +1000

    UI progress

--- as64ui/dialog/base_window.py
+++ as64ui/dialog/base_window.py
@@ -36,25 +36,38 @@ class BaseWindow(QDialog):
         # Layouts
         self._layout = QVBoxLayout(self)
         self._layout.setContentsMargins(0, 0, 0, 0)
+        self._layout.setSpacing(0)
+
+        self._top_layout = QVBoxLayout()
+        self._top_layout.setContentsMargins(20, 20, 20, 20)
+        self._top_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
-        self._bottom_layout = QHBoxLayout(self)
+        self._bottom_layout = QHBoxLayout()
         self._bottom_layout.setContentsMargins(0, 0, 0, 0)
         
-        self._left_layout = QVBoxLayout(self)
-        self._left_layout.setContentsMargins(0, 0, 0, 0)
+        self._bottom_left_layout = QVBoxLayout()
+        self._bottom_left_layout.setContentsMargins(0, 0, 0, 0)
 
-        self._right_layout = QVBoxLayout(self)
-        self._right_layout.setContentsMargins(0, 0, 0, 0)
-        self._right_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight)
+        self._bottom_right_layout = QVBoxLayout()
+        self._bottom_right_layout.setContentsMargins(0, 0, 0, 0)
+        self._bottom_right_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight)
         
         # Widgets
         self._side_menu = SideMenu(self)
+
         self._route_title = QLineEdit(self)
+        self._route_title.setFixedHeight(30)
         
         # Populate Layouts
-        self._left_layout.addWidget(self._side_menu, alignment=Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignLeft)
+        self._bottom_left_layout.addWidget(self._side_menu, alignment=Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignLeft)
         
-        self._layout.addLayout(self._left_layout)
+        self._bottom_layout.addLayout(self._bottom_left_layout)
+        self._bottom_layout.addLayout(self._bottom_right_layout)
+
+        self._top_layout.addWidget(self._route_title, alignment=Qt.AlignmentFlag.AlignVCenter)
+
+        self._layout.addLayout(self._top_layout)
+        self._layout.addLayout(self._bottom_layout)
         
     def add_menu_option(self, pixmap: QPixmap, text: str) -> None:
         self._side_menu.add_option(pixmap, text)
