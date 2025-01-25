24204a666b8301c0abe0295402e9b54e9922a731
Kaine <kainevangemert@gmail.com>
2025-01-26 09:48:53 +1100

    Update automated header

--- as64/enums.py
+++ as64/enums.py
@@ -1,3 +1,12 @@
+# AutoSplit64
+#
+# Copyright (C) 2025 Kainev
+#
+# This project is currently not open source and is under active development.
+# You may view the code, but it is not licensed for distribution, modification, or use at this time.
+#
+# For more information see https://github.com/Kainev/AutoSplit64?tab=readme#license
+
 from enum import Enum, Flag, auto
 
 
