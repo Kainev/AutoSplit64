8057fc1653bfe33a4229de46a659bc7c6e1b6af5
Kaine <kainevangemert@gmail.com>
2025-02-09 14:19:35 +1100

    Add base windows helper functions

--- as64/helpers/windows.py
+++ as64/helpers/windows.py
@@ -1,11 +1,22 @@
+# AutoSplit64
+#
+# Copyright (C) 2025 Kainev
+#
+# This project is currently not open source and is under active development.
+# You may view the code, but it is not licensed for distribution, modification, or use at this time.
+#
+# For more information see https://github.com/Kainev/AutoSplit64?tab=readme#license
+
+import os
 import win32gui
 import win32process
+import win32api
+import win32con
 import psutil
 
 from as64.ipc import rpc
 
 def enum_window_callback(hwnd, pid_set):
-        # Check if the window is visible and has a non-empty title
         if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd):
             _, pid = win32process.GetWindowThreadProcessId(hwnd)
             pid_set.add(pid)
@@ -13,10 +24,9 @@ def enum_window_callback(hwnd, pid_set):
 @rpc.register("windows.visible_process_names")
 def visible_process_names():
     pid_set = set()
-    # Enumerate all top-level windows
+    # Enumerate top-level windows
     win32gui.EnumWindows(enum_window_callback, pid_set)
     
-    # Get psutil.Process objects for each pid
     processes = []
     for pid in pid_set:
         try:
