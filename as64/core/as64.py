e79d3b3634d0eba030e6a690833cded8d9b3094f
Kaine <kainevangemert@gmail.com>
2025-02-11 14:57:28 +1100

    Add TimedEvent

--- as64/core/as64.py
+++ as64/core/as64.py
@@ -8,6 +8,8 @@
 # For more information see https://github.com/Kainev/AutoSplit64?tab=readme#license
 
 import time
+from dataclasses import dataclass
+from typing import Optional
 
 from as64 import (
     config,
@@ -16,8 +18,15 @@ from as64 import (
 from as64.core.route import Route, Split, load as load_route
 from as64.core.capture import GameCapture
 
-from as64.enums import Version, FadeStatus
-from as64.plugins import PluginManager, SplitPlugin
+from as64.enums import Version, FadeStatus, Camera, AS64Status
+from as64.plugins import PluginManager, SplitPlugin, CapturePlugin
+
+from as64 import api
+
+@dataclass
+class TimedEvent:
+    started: float = 0.0
+    stopped: float = 0.0
 
 
 class GameState(object):
@@ -28,15 +37,19 @@ class GameState(object):
         self.last_fade_out_time: float = 0  # Time the last fade-out began
         self.last_fade_in_time: float = 0   # Time the last fade-in began
         self.last_reset_time: float = 0     # Time the last reset occured
-        self.last_x_cam_time: float = 0     # Time the last x-cam began
         self.delta: float = 0               # Amount of time the last frame took to execute
         
+        self.x_cam = TimedEvent()
+        self.mario_cam = TimedEvent()
+        self.lakitu_cam = TimedEvent()
+        self.save_menu = TimedEvent()
+                
         # Game Status
         self.star_count: int = None
         self.fade_out_count: int = 0
         self.fade_in_count: int = 0
         self.x_cam_count: int = 0
-        self.in_x_cam: bool = False
+        self.camera: Optional[Camera] = None
         self.in_bowser_fight: bool = False
         self.fade_status = FadeStatus.NO_FADE
         self.in_intro: bool = True
@@ -55,16 +68,14 @@ class GameState(object):
         self.frame = game_capture.region_image
         self.region = game_capture.region_rect
         
-    # def _register_game_capture(game_capture: GameCapture)
-    #     self.frame
-
+        
 class GameController(object):
     def __init__(self) -> None:
         self.fps: float = 29.97
         self.predict_star_count: bool = True
-        self.count_fades: bool = True
+        self.count_fades: bool = False
         self.count_x_cams: bool = False
-        self.allow_star_jump: bool = True
+        self.allow_star_jump: bool = False
        
         self.undo = None
         self.skip = None
@@ -78,43 +89,81 @@ class GameController(object):
         self.reset = split_plugin.reset
 
 class AS64(object):
-    def __init__(self, plugin_manager: PluginManager, event_emitter) -> None:
+    def __init__(self, plugin_manager: PluginManager, enqueue_message) -> None:
         self._plugin_manager = plugin_manager
-        self._plugin_manager.instantiate_game_state_plugins()
+        self._enqueue_message = enqueue_message
         
         self._route = load_route(config.get('route', 'path'))
-        
-        self._game_capture: GameCapture = GameCapture(Version.JP, config.get('capture', 'region'),  plugin_manager.get_capture_instance())
-        self._game_status = GameState(self._route, self._game_capture)
+                
+        self._capture_plugin: CapturePlugin = plugin_manager.get_active_plugin_classes(api.CapturePlugin)()           
+            
+        self._game_capture: GameCapture = GameCapture(Version.JP, config.get('capture', 'region'),  self._capture_plugin)
+        self._game_state = GameState(self._route, self._game_capture)
         self._game_controller = GameController()
         
-        self._split_plugin: SplitPlugin = plugin_manager.get_split_instance(self._game_status)
+        # Register with API
+        api.get_game_controller = self._get_game_controller
+        api.get_game_state = self._get_game_state
+        
+        self._split_plugin: SplitPlugin = plugin_manager.get_active_plugin_classes(api.SplitPlugin)(self._game_state)
         
         self._game_controller._register_split_plugin(self._split_plugin)
-        self._plugin_manager.initialize_game_state_plugins(self._game_status, self._game_controller)
-        self._plugin_manager.start_lifecycle_plugins()
+        self._plugin_manager.instantiate_plugins(api.GameStatePlugin)
+        self._plugin_manager.run_method(api.GameStatePlugin, "initialize", self._game_state, self._game_controller)
         
+        self._game_state_plugins = plugin_manager.get_plugin_instances(api.GameStatePlugin)
+        self._realtime_plugins = [plugin for plugin in plugin_manager.get_plugin_instances(api.Plugin) if plugin.is_realtime]
         
-    def run(self) -> None:
-        self._game_status.current_time = time.perf_counter()
+    def is_valid(self):
+        if not self._capture_plugin.is_valid():
+            return False
+        if not self._split_plugin.is_valid():
+            return False
         
-        # Capture the current frame
-        self._game_capture.capture()
+        return True
         
-        # Sync Split Plugin
-        self._split_plugin.sync()
+    def _get_game_controller(self) -> GameController:
+        return self._game_controller
+    
+    def _get_game_state(self) -> GameState:
+        return self._game_state
+    
+    def _on_start(self) -> None:
+        self._plugin_manager.run_method(api.GameStatePlugin, "start", self._game_state, self._game_controller)
+        self._plugin_manager.run_method(api.Plugin, "start", self._game_state, self._game_controller)
         
-        # Execute GameState plugins
-        for plugin in self._plugin_manager.game_state_plugins:
-            plugin.execute(self._game_status, self._game_controller)
+    def _on_stop(self) -> None:
+        self._plugin_manager.run_method(api.Plugin, "stop")
+        
+        self._plugin_manager.run_method(api.GameStatePlugin, "stop")
+        self._plugin_manager.run_method(api.GameStatePlugin, "shutdown")
+        
+    def run(self, stop_event) -> None:
+        self._on_start()
+        self._enqueue_message({"event": "status", "data": AS64Status.RUNNING.value})
+        
+        while not stop_event.is_set():
+            self._game_state.current_time = time.perf_counter()
             
-        # Execute Real Time Plugins
-        for plugin in self._plugin_manager.realtime_plugins:
-            plugin.execute(None)
-                        
-        # Limit FPS
-        try:
-            self._game_status.delta = time.perf_counter() - self._game_status.current_time
-            time.sleep(1 / self._game_controller.fps - self._game_status.delta)
-        except ValueError:
-            pass
+            # Capture the current frame
+            self._game_capture.capture()
+            
+            # Sync Split Plugin
+            self._split_plugin.sync()
+            
+            # Execute GameState plugins
+            for plugin in self._game_state_plugins:
+                plugin.execute(self._game_state, self._game_controller)
+
+            # # Execute Real Time Plugins
+            for plugin in self._realtime_plugins:
+                plugin.execute(self._game_state, self._game_controller)
+
+            # Limit FPS
+            try:
+                self._game_state.delta = time.perf_counter() - self._game_state.current_time
+                time.sleep(1 / self._game_controller.fps - self._game_state.delta)
+            except ValueError:
+                pass
+
+        self._on_stop()
